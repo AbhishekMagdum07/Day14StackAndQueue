bea1b6f2a93a06de779dbee5a322e83dd011d954
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-11 15:03:36 +0530

    Added: Peek and pop the elements from stack using linked list

--- src/com/stack_and_queue/StackAndQueue.java
+++ src/com/stack_and_queue/StackAndQueue.java
@@ -12,6 +12,7 @@ public class StackAndQueue {
             size++;
         }
     }
+    // Uc1 = push elements in stack.
     public void pushElementInStack(Object data){   // creating push method in stack
         Node newNode = new Node(data);
         if(head == null){                          // check stack is empty or not
@@ -24,6 +25,25 @@ public class StackAndQueue {
         }
         temp.next = newNode;
     }
+    //uc2 = pop elements from the stack.
+    public void popElementsFromStack(){
+        if(head == null){
+            System.out.println("Stack is empty");
+            return;
+        }
+        if(head.next == null){                  // if stack  has only one element then this block will execute.
+            head = null;
+            return;
+        }
+        Node lasttemp = head.next;
+        Node secondLastTemp = head;
+        while (lasttemp.next != null) {
+            lasttemp = lasttemp.next;           // traverse till top of the stack
+            secondLastTemp = secondLastTemp.next;
+        }
+        secondLastTemp.next = null;            // delete the element
+    }
+    // display the stack elements.
     public void displayElements(){
         if(head == null){
             System.out.println("Linked list is empty");
@@ -37,12 +57,14 @@ public class StackAndQueue {
         }
         System.out.print("Null");
     }
-
     public static void main(String[] args) {
         StackAndQueue runner = new StackAndQueue();
         runner.pushElementInStack(70);
         runner.pushElementInStack(30);
         runner.pushElementInStack(56);
+        runner.popElementsFromStack();
+        runner.popElementsFromStack();
+        runner.popElementsFromStack();
         runner.displayElements();
     }
 }
